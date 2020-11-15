53fb720d986021f91462f5af0c181ae244685d49
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2020-11-15 16:04:39 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,15 +5,10 @@ variable private_key_pem {
 
 variable "hosts" {
   type = map(string)
-
-  default = {
-    "localhost" = "127.0.0.1"
-  }
   description = "map of hosts"
 }
 
 variable "domain" {
   type        = string
-  default     = "localdomain"
   description = "domain to complete FQDN"
 }
